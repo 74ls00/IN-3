0fa28470c413d281a584a0bf284f705105b3958f
74ls00 <userled@yandex.ru>
2019-01-21 22:46:34 +0300

    .

--- arduino/_595fw-13-8-6/_595fw-13-8-6.ino
+++ arduino/_595fw-13-8-6/_595fw-13-8-6.ino
@@ -4,6 +4,9 @@ int latchPin = 10;//сигнал Ready
 int clockPin = 12;//сигнал Clock
 int dataPin = 11;//cигнал Data
 
+int tmp;
+
+
 int x1;
 int x2;
 int x3;
@@ -13,11 +16,13 @@ int pot;
      
 float display = 1112; //величина, выводимая на индикатор = 4 цифры
 
+
+
 //const byte digit[] =      //13 segment digits in bits
 const unsigned int digit[] = {
   4095, //0      111111111111
   124,  //1           1111100
-  6111, //2     1011111011111
+  6111, //2     1011111011111 17DF
 //5631, //3     1010111111111
   4607, //3 alt 1000111111111
   7293, //4     1110001111101
@@ -58,84 +63,112 @@ void loop(){
 //http://arduino.ru/forum/pesochnitsa-razdel-dlya-novichkov/razdelit-chislo-na-8-bitnye
 
 uint64_t chislo=0;
-chislo+=digit[1];
-Serial.println(lowByte(chislo),BIN);
-chislo<<13;
-Serial.println(lowByte(chislo),BIN);
-//chislo+=digit[2];
-//chislo<<13;
-//Serial.println(lowByte(chislo),BIN);
-//chislo+=digit[3];
-//chislo<<13;
-//Serial.println(lowByte(chislo),BIN);
-//chislo+=digit[4];
-//Serial.println(lowByte(chislo),BIN);
+//chislo+=digit[1];
+//chislo+=8191;4145
+//chislo+=4145;
+chislo+=124;  //1  0000001111100
 
+
+unsigned long long1 = (unsigned long)((chislo & 0xFFFF0000) >> 16 );
+unsigned long long2 = (unsigned long)((chislo & 0x0000FFFF));
+String hex = String(long1, HEX) + String(long2, HEX); // six octets
+//Serial.println(hex);
+
+chislo = chislo<<13;
+// chislo+=digit[2];
+chislo+=0;
+
+chislo = chislo<<13;
+//chislo+=digit[3]; // 3  разряд
+chislo+=6371; // 1100011100011
+
+chislo = chislo<<13;
+//chislo+=digit[4]; // 4 разряд
+//chislo+=8191; // 4 разряд
+//chislo+=5887;
+chislo+=5881; // 1011011111001
+
+//chislo = chislo<<4; // 4 неиспользуемых порта
+
+
+unsigned long long3 = (unsigned long)((chislo & 0xFFFF0000) >> 16 );
+unsigned long long4 = (unsigned long)((chislo & 0x0000FFFF));
+String hex2 = String(long3, HEX) + String(long4, HEX); // six octets
+Serial.println(hex2);
+
+/*
+uint64_t chislo=0;
+uint8_t resultat[7];
+for (byte i=0; i<4; i++){
+chislo<<13;
+chislo+=digit[i];
+}
+*/
      
           digitalWrite(latchPin, LOW);
 
-Serial.println(lowByte(chislo),BIN);
-shiftOut(dataPin, clockPin, MSBFIRST, lowByte(chislo));
-
-
-chislo>>5;
 //Serial.println(lowByte(chislo),BIN);
 
-shiftOut(dataPin, clockPin, MSBFIRST, lowByte(chislo));
 
 
 
-//chislo>>13;
-//shiftOut(dataPin, clockPin, MSBFIRST, lowByte(chislo));
-
-//chislo>>13;
-//shiftOut(dataPin, clockPin, MSBFIRST, lowByte(chislo));
-
-    //shiftOut(dataPin, clockPin, MSBFIRST, lowByte(digit[x4]));
 
 
+tmp = lowByte(chislo);
+//chislo = chislo>>8;
+Serial.println(lowByte(chislo),BIN);
+shiftOut(dataPin, clockPin, LSBFIRST, lowByte(chislo));
+//Serial.println(tmp,BIN);
+ // shiftOut(dataPin, clockPin, MSBFIRST, tmp);
 
-     /*
-    shiftOut(dataPin, clockPin, MSBFIRST, highByte(digit[x1]));
-    shiftOut(dataPin, clockPin, MSBFIRST, lowByte(digit[x1]));
+chislo = chislo>>8;
 
-    shiftOut(dataPin, clockPin, MSBFIRST, highByte(digit[x2]));
-    shiftOut(dataPin, clockPin, MSBFIRST, lowByte(digit[x2]));
+Serial.println(lowByte(chislo),BIN);
+shiftOut(dataPin, clockPin, LSBFIRST, lowByte(chislo));
 
-    shiftOut(dataPin, clockPin, MSBFIRST, highByte(digit[x3]));
-    shiftOut(dataPin, clockPin, MSBFIRST, lowByte(digit[x3]));
-*/
+
+chislo = chislo>>8;
+
+Serial.println(lowByte(chislo),BIN);
+shiftOut(dataPin, clockPin, LSBFIRST, lowByte(chislo));
+
+
+chislo = chislo>>8;
+
+Serial.println(lowByte(chislo),BIN);
+shiftOut(dataPin, clockPin, LSBFIRST, lowByte(chislo));
+
+
+
 
 /*
-Serial.println( digit[x3]);
-Serial.println( digit[x3], BIN);
-Serial.println( lowByte(digit[x3]),BIN);
-Serial.println( highByte(digit[x3]),BIN);
-    */
+tmp = lowByte(chislo);
+chislo = chislo>>8;
+Serial.println(lowByte(chislo),BIN);
+shiftOut(dataPin, clockPin, MSBFIRST, lowByte(chislo));
+Serial.println(tmp,BIN);
+shiftOut(dataPin, clockPin, MSBFIRST, tmp);
 
 
-  //  shiftOut(dataPin, clockPin, MSBFIRST, highByte(digit[x4]));
-  //  shiftOut(dataPin, clockPin, MSBFIRST, lowByte(digit[x4]));
-
-    
-    /*
-Serial.println( digit[x4]);
-Serial.println( digit[x4], BIN);
-Serial.println( lowByte(digit[x4]),BIN);
-Serial.println( highByte(digit[x4]),BIN);
+tmp = lowByte(chislo);
+chislo = chislo>>8;
+Serial.println(lowByte(chislo),BIN);
+shiftOut(dataPin, clockPin, MSBFIRST, lowByte(chislo));
+Serial.println(tmp,BIN);
+shiftOut(dataPin, clockPin, MSBFIRST, tmp);
 */
 
 
-        //  shiftOut(dataPin, clockPin, MSBFIRST, digit[x4]>>8);
+
+
+//---------------------------------
+
+
 
           digitalWrite(latchPin, HIGH);
 
 
-
-
-
-
-  delay(500);  
+  delay(1000);  
 }//конец цикла
 
 
